9695ca9cc593bc9c7b8ba1ce0e5e1e60667baba4
shner-elmo <770elmo@gmail.com>
2022-11-05 21:32:10 +0100

    Removed Shebang

--- pandasdb/__init__.py
+++ pandasdb/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 from .connection import Database
 from .utils import concat
 from .expression import Expression  # to pass a custom Expression object in Table.filter or Column.filter
